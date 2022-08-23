7b90ccb0361ba845914f5ebcbff44d255651daa3
hammad <hammadbhat126@gmail.com>
2022-08-23 19:50:12 +0400

    permissions

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weather_flutter/utilities/constants.dart';
+import 'package:clima/utilities/constants.dart';
 
 class LocationScreen extends StatefulWidget {
   @override
